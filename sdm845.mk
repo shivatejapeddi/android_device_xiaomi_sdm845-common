8d2cdfab8b85687f98e5f1c683e0fb04112c0b49
Amit Shekhar <ashekhar@codeaurora.org>
2017-04-03 15:49:55 -0700

    sdm845-common: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    mediacodec: allow getdents64, sysinfo and getcwd
    
    Vendor extended policy files are separated from the aosp
    and will now reside in /vendor
    
    Change-Id: If59d04b36d744efb74209ae36e76dd16cb18f606
    CRs-Fixed: 2027600

--- sdm845.mk
+++ sdm845.mk
@@ -113,6 +113,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH)
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
