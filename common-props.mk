e2d5d84dc093d0f06c4e0bb00e72242d901fb232
shivatejapeddi <shivatejapeddi@gmail.com>
2019-11-18 10:05:30 +0000

    Revert "sdm845-common: Add assertions on modem version"
    This reverts commit 779c50b39f3e2498c17def476104cd24cbacc610.

--- common-props.mk
+++ common-props.mk
@@ -74,7 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-    debug.sf.enable_gl_backpressure=1
+    debug.sf.enable_gl_backpressure=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Media
