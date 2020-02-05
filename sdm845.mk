8091aaf0c36cb0ca76c34cbf0b79bf038e810e9e
kubersharma001 <kubersharma001@gmail.com>
2020-02-05 22:56:53 +0000

    sdm845-common: Do not enable updatable APEXes on every target
    This reverts commit 52a78a2af1f269d652b72761ecc0bf15e7fc306c.
    
    It can't really be enabled for beryllium, as apexd requires /data/apex
    to be available right after boot to update all the Android modules.
    As in all FDE devices, /data/apex is encrypted until the device
    is unlocked by the user, rendering APEX basically useless as only
    the system APEX variants will be loaded at boot.
    
    test:
    beryllium(FDE):
    Boots with forceencryption after a clean install.
    Boots after wiping data, system and flash over MIUI 11 - Q encrypted.
    
    polaris(FBE):
    Unaffected, everything still works.
    
    Change-Id: I0649862e5c5b2c1a1cb597914a11df79b3e01957
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
 
